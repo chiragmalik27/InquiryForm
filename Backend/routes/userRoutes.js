a1a7872ed1a3a1aba05e21082cda8447718ea043
Kartik Aslia <s7011068163@gmail.com>
2024-01-23 00:24:19 +0530

    dono

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -5,11 +5,11 @@ const router = express.Router();
 let inquiries = [];
 
 router.get('/', function(req, res){
-    res.json("Hello World").sendStatus(200);
+    res.status(200).json("Hello World");
 });
 
 router.get('/login', function(req, res){
-    res.status(200).sendFile(path.join(__dirname, '../../Frontend/login/loginpage.html'));
+    res.sendFile(path.join(__dirname, '../../Frontend/login/loginpage.html'));
 });
 
 router.get('/singup', function(req, res){
@@ -19,11 +19,11 @@ router.get('/singup', function(req, res){
 router.post('/inquiry', function(req, res){
     const inquiry = req.body;
     inquiries.push(inquiry);
-    res.status(200).send('Inquiry received');
+    res.send('Inquiry received');
 });
 
 router.get('/inquiries', (req, res) => {
-    res.status(200).json(inquiries);
+    res.json(inquiries);
 });
 
 module.exports = router;
